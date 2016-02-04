0ab6023ac1283b9b77187f3ebf9995ea86282d8a
Alessandro Guaresti <alessguar@gmail.com>
2016-02-03 16:39:46 -0800

    adding things
    Im a trash panda

--- core/src/com/sshsgd/mlgbreakout/entities/Ball.java
+++ core/src/com/sshsgd/mlgbreakout/entities/Ball.java
@@ -2,9 +2,19 @@ package com.sshsgd.mlgbreakout.entities;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 
 public class Ball extends Entity {
 
+	private float radians;
+	
+	private Vector2 vel;
+	private Vector2 spawn;
+	
+	public Ball(){
+		
+	}
+	
 	@Override
 	public void draw(ShapeRenderer sr, SpriteBatch sb, float dt) {
 		// TODO Auto-generated method stub
